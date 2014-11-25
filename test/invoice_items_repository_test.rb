b44ba52c319c224571cf424133af788e417777d0
Brandon Mrsny <bmrsny@gmail.com>
2014-11-25 10:14:53 -0700

    refactored repository test

--- test/invoice_items_repository_test.rb
+++ test/invoice_items_repository_test.rb
@@ -2,7 +2,6 @@ require_relative 'test_helper'
 require_relative '../lib/invoice_items_repository'
 
 class InvoiceItemsRepositoryTest < Minitest::Test
-
   def test_can_retrieve_attributes
     invoice_items = [
       {id: "25", item_id: "928", invoice_id: "5", quantity: "9", unit_price: "32346", created_at: "2012-03-27 14:54:10 UTC", updated_at: "2012-03-27 14:54:10 UTC" },
