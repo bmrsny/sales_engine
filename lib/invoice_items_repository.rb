bcfafe88642a2ea578349cb6f4de2863a4c06097
Chris Luhring <cluhring@gmail.com>
2014-11-25 00:44:50 -0700

    search function added to invoice items & transactions classes

---
+++ lib/invoice_items_repository.rb
@@ -0,0 +1,70 @@
+require_relative 'invoice_items'
+
+class InvoiceItemsRepository
+  attr_reader :invoice_items
+
+  def initialize(invoice_items)
+    @invoice_items = invoice_items
+  end
+
+  def find_by_id(x)
+    invoice_items.select do |invoice_item|
+      invoice_item.id == x
+    end
+  end
+
+  def find_by_item_id(x)
+    invoice_items.select do |invoice_item|
+      invoice_item.item_id == x
+    end
+  end
+
+  def find_by_invoice_id(x)
+    invoice_items.select do |invoice_item|
+      invoice_item.invoice_id == x
+    end
+  end
+
+  def find_by_quantity(x)
+    invoice_items.select do |invoice_item|
+      invoice_item.quantity == x
+    end
+  end
+
+  def find_by_unit_price(x)
+    invoice_items.select do |invoice_item|
+      invoice_item.unit_price == x
+    end
+  end
+
+  # def find_all_by_id(x)
+  #   invoice_items.select do |invoice_item|
+  #     invoice_item.all_by_id == x
+  #   end
+  # end
+  #
+  # def find_all_by_item_id(x)
+  #   invoice_items.select do |invoice_item|
+  #     invoice_item.all_by_item_id == x
+  #   end
+  # end
+  #
+  # def find_all_by_invoice_id(x)
+  #   invoice_items.select do |invoice_item|
+  #     invoice_item.all_by_invoice_id == x
+  #   end
+  # end
+  #
+  # def find_all_by_quantity(x)
+  #   invoice_items.select do |invoice_item|
+  #     invoice_item.all_by_quantity == x
+  #   end
+  # end
+  #
+  # def find_all_by_unit_price(x)
+  #   invoice_items.select do |invoice_item|
+  #     invoice_item.all_by_unit_price == x
+  #   end
+  # end
+
+end
